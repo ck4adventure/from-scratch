373b13d1555bcbf3deebdd4fe31a6ba0bf64c9d0
Christina Kiesz <ckiesz@gmail.com>
2020-12-17 14:04:14 -0800

    add note on todo

--- app/graphql/mutations/add_recipe.rb
+++ app/graphql/mutations/add_recipe.rb
@@ -9,7 +9,7 @@ module Mutations
     argument :steps, String, required: false
     argument :base_item, Boolean, required: false
 
-    # TODO: define resolve method
+    # TODO: define resolve method to use params hash
     def resolve(title:, description:, steps:, base_item:)
       recipe = Recipe.new(title: title, description: description, steps: steps, base_item: base_item)
       if recipe.save!
